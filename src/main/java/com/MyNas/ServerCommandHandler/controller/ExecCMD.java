4b652a0a9306e8d861d86d087636c6779e085d17
Amady Diagne <amady6.add@gmail.com>
2020-05-28 01:10:45 +0200

    ExecCMD update

--- src/main/java/com/MyNas/ServerCommandHandler/controller/ExecCMD.java
+++ src/main/java/com/MyNas/ServerCommandHandler/controller/ExecCMD.java
@@ -4,6 +4,7 @@
 package com.MyNas.ServerCommandHandler.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExecCMD {
 	ProcessBuilder processBuilder = new ProcessBuilder();
 	
+	
+	/*
+	 * Initialise un répertoire au niveau du serveur ftp pour le user
+	 */
 	@PostMapping(value = "/initUserDir")
 	public String initUserDirectory(@RequestParam String userName) {
 		
@@ -39,6 +44,43 @@ public class ExecCMD {
 
 		return result;
 	}
+	
+	/*
+	 *  permet de rajouter un ou plusieurs sous-répertoires pour le user
+	 *  au niveau du serveur ftp
+	 */
+	@PostMapping(value = "/addFolder")
+	public String addSubFolder(@RequestParam String userName, @RequestParam List<String> folders) {
+		
+		String result ="";
+		String finalFolder = "/home/ftpuser/ftp/"+userName;
+		if(! folders.isEmpty()) {
+			for(String folder : folders) {
+				finalFolder = finalFolder+"/"+folder;
+			}
+		}else {
+			return "Erreur: pas de répertoire fourni";
+		}
+		
+		String firstCmdToRun = "mkdir "+finalFolder;
+		String secondCmdToRun = "sudo chown ftpuser:ftpuser "+finalFolder;
+		
+		try {
+			executeCommande(firstCmdToRun);
+			executeCommande(secondCmdToRun);
+			result = "Répertoire créé";
+		}catch(Exception e) {
+			e.printStackTrace();
+			result ="Erreur: Répertoire non créé";
+		}
+		
+		return result;
+	}
+	
+	
+	/*
+	 *  Permet d'exécuter la commande passer en argument au niveau du serveur linux
+	 */
 	
 	public void executeCommande(String commande) {
 		processBuilder.command("bash", "-c", commande);
